79007e27972252dd250e1a76fda390acabcb89f6
amasin76 <51626916+amasin76@users.noreply.github.com>
2022-06-24 00:29:26 +0100

    Add files via upload

---
+++ Bulletien_De_Paie/Reports/formBulletienReport.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class formBulletienReport
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim ReportDataSource1 As Microsoft.Reporting.WinForms.ReportDataSource = New Microsoft.Reporting.WinForms.ReportDataSource()
+        Me.BulletienWithEmployeNameBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.employee_dbDataSet = New Bulletien_De_Paie.employee_dbDataSet()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.BulletienWithEmployeNameTableAdapter = New Bulletien_De_Paie.employee_dbDataSetTableAdapters.BulletienWithEmployeNameTableAdapter()
+        CType(Me.BulletienWithEmployeNameBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.employee_dbDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'BulletienWithEmployeNameBindingSource
+        '
+        Me.BulletienWithEmployeNameBindingSource.DataMember = "BulletienWithEmployeName"
+        Me.BulletienWithEmployeNameBindingSource.DataSource = Me.employee_dbDataSet
+        '
+        'employee_dbDataSet
+        '
+        Me.employee_dbDataSet.DataSetName = "employee_dbDataSet"
+        Me.employee_dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill
+        ReportDataSource1.Name = "DataSet1"
+        ReportDataSource1.Value = Me.BulletienWithEmployeNameBindingSource
+        Me.ReportViewer1.LocalReport.DataSources.Add(ReportDataSource1)
+        Me.ReportViewer1.LocalReport.ReportEmbeddedResource = "Bulletien_De_Paie.BulletiensAsGrid.rdlc"
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer1"
+        Me.ReportViewer1.Size = New System.Drawing.Size(936, 827)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'BulletienWithEmployeNameTableAdapter
+        '
+        Me.BulletienWithEmployeNameTableAdapter.ClearBeforeFill = True
+        '
+        'formBulletienReport
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(936, 827)
+        Me.Controls.Add(Me.ReportViewer1)
+        Me.Name = "formBulletienReport"
+        Me.Text = "formBulletienReport"
+        CType(Me.BulletienWithEmployeNameBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.employee_dbDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents BulletienWithEmployeNameBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents employee_dbDataSet As Bulletien_De_Paie.employee_dbDataSet
+    Friend WithEvents BulletienWithEmployeNameTableAdapter As Bulletien_De_Paie.employee_dbDataSetTableAdapters.BulletienWithEmployeNameTableAdapter
+End Class
---
+++ Bulletien_De_Paie/Reports/formBulletienReport.vb
@@ -0,0 +1,16 @@
+﻿Public Class formBulletienReport
+
+    Private Sub formBulletienReport_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        CenterToScreen()
+        'TODO: This line of code loads data into the 'employee_dbDataSet.BulletienWithEmployeName' table. You can move, or remove it, as needed.
+        Me.BulletienWithEmployeNameTableAdapter.Fill(Me.employee_dbDataSet.BulletienWithEmployeName)
+        If formBulletien.Zsearch.Text = "" Then
+            Me.BulletienWithEmployeNameTableAdapter.Fill(Me.employee_dbDataSet.BulletienWithEmployeName)
+        Else
+            Me.BulletienWithEmployeNameTableAdapter.FillBy(Me.employee_dbDataSet.BulletienWithEmployeName, formBulletien.Zsearch.Text)
+        End If
+        ReportViewer1.ZoomMode = Microsoft.Reporting.WinForms.ZoomMode.PageWidth
+
+        Me.ReportViewer1.RefreshReport()
+    End Sub
+End Class
